813d4e9a1891813d4c0b70a126c5ce11ee8c40e4
Vagesh Vora <vageshvora12@gmail.com>
2022-03-20 17:32:47 +0530

    stats api integrated

--- backend/index.js
+++ backend/index.js
@@ -155,6 +155,62 @@ app.get('/game/:id',(req,res) =>{
 })
 
 
+// ?seasons[]=2018&seasons[]=2015&player_ids[]=1&player_ids[]=2&postseason=true will return stats for player_ids 1 and 2 for the 2015-2016 and 2018-2019 postseason
+// get data for all stats
+
+
+app.get('/stats/all-stats',(req,res) =>{
+    axios.get('https://www.balldontlie.io/api/v1/stats')
+    .then(function(response){
+        console.log(response.data.data);
+        let data= '';
+        for(i=0;i<response.data.data.length;i++){
+            // data += 'TEAM '+ (i+1) + "<br>TEAM ID: " + response.data.data[i].id + '<br>DATE:' + response.data.data[i].date + '<br>DATE:' + response.data.data[i].date + '<br>FULL NAME:' + response.data.data[i].full_name+ "<br>NAME: " + response.data.data[i].name + '<br>ABBREVIATION: ' + response.data.data[i].abbreviation + '<br>CITY: ' + response.data.data[i].city + '<br>CONFERENCE: ' + response.data.data[i].conference + '<br>DIVISION: ' + response.data.data[i].division + '<br><br><br>';
+            data += '<br>' + JSON.stringify(response.data.data[i]) + '<br>'  ;
+        }
+        res.send(data);
+    })
+    .catch(function (err) {
+        console.log(err);
+        })
+        .then(function(){
+            console.log('Process Executed');
+        })
+})
+
+// example for combined queries eg for seasons and players
+// ?seasons[]=2018&seasons[]=2015&player_ids[]=1&player_ids[]=2&postseason=true
+// get a specific stat 
+
+app.get('/statistics',(req,res) =>{
+    axios.get('https://www.balldontlie.io/api/v1/stats?seasons[]=2018&seasons[]=2015')
+    .then(function(response){
+        // console.log(response.data.data);
+        let data = '';
+        for(i=0;i<response.data.data.length;i++){
+            data += 'Stats for seasons 2018 & 2015<br><br>' + JSON.stringify(response.data.data[i]) + '<br>'  ;
+        }
+        res.send(data);
+    })
+    .catch(function (err) {
+        console.log(err);
+        })
+        .then(function(){
+            console.log('Process Executed');
+        })
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
