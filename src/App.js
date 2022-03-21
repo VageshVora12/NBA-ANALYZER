b1812d335845c2be4cf44ee2d4fce9272962544b
Vagesh Vora <vageshvora12@gmail.com>
2022-03-21 15:28:05 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="topnav" style={{backgroundColor:'#333',overflow:'hidden'}}>
+  <a class="active" href="#home" style={{float:'left',color:'#2f2f2',textAlign:'center',padding:'14px 16px',textDecoration:'none',fontSize:'17px'}}>Home</a>
+  <a href="#news" style={{float:'left',color:'#2f2f2',textAlign:'center',padding:'14px 16px',textDecoration:'none',fontSize:'17px'}}>News</a>
+  <a href="#contact" style={{float:'left',color:'#2f2f2',textAlign:'center',padding:'14px 16px',textDecoration:'none',fontSize:'17px'}}>Contact</a>
+  <a href="#about" style={{float:'left',color:'#2f2f2',textAlign:'center',padding:'14px 16px',textDecoration:'none',fontSize:'17px'}}>About</a>
+</div>
+      <h2><p style={{color:"skyblue"}}>This is NBA Analayzer</p></h2>
+  
     </div>
+    <a href="http://localhost:9000/statistics">ALL STATS</a><br></br>
+      <a href="http://localhost:9000/game/all-games">ALL GAMES</a><br></br>
+      <a href="http://localhost:9000/player/all-players">ALL PLAYERS</a><br></br>
+      <a href="http://localhost:9000/team/all-teams">ALL TEAMS</a><br></br>
+
+    </>
   );
 }
 
