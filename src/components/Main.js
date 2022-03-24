bf4883e8f2482d6d311a2da536f3379f5a1c308d
Vagesh Vora <vageshvora12@gmail.com>
2022-03-25 03:21:04 +0530

    deploy

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const Main = () =>{
-    return <div> <p style={{marginTop:'100px'}}>Main Screen</p></div>
+
+      
+
+return (<div> <p style={{marginTop:'50px',color:'blue'}}>Main Screen</p>
+    <h2 style={{color:'royalblue'}}>NBA Analyzer</h2>
+
+    
+    </div>)
 }
 export default Main;
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -5,26 +5,10 @@ import {getStats} from '../api/Api';
 const Stats = ()=> {
 
 
-
-    useEffect(() => { 
-      getStats()
-       .then((res) => {
-         console.log(res.data);
-         // console.log(game);
-        })
-        .catch((error) => console.log('ERROR: could not load data',error))
-      }, []);
-
-
-
     return (
         <div>
         <p style={{marginTop:'200px'}}>STATS</p>
     <div>
-        {getStats().then((res)=>{
-            res.send(res.data)
-
-        })}
     </div>
         
         </div>)
