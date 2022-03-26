e723657e4ac09fa4a24a8adbb6a1e2c4562e8b98
Vagesh Vora <vageshvora12@gmail.com>
2022-03-26 14:46:15 +0530

    Heroku taking load cant load under 60 secs

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Teams from './TeamPage';
+// import Teams from './TeamPage';
 const Main = () =>{
 
       
 
 return (<div> <p style={{marginTop:'50px',color:'blue'}}>Main Screen</p>
     <h2 style={{color:'royalblue'}}>NBA Analyzer</h2>
-    <Teams />
+    {/* <Teams /> */}
 
     
     </div>)
